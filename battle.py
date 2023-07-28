af1883fe20d0b5d9b1a7514cf3208ca5ffff2f28
Montazu <montazu@montazu.pl>
2023-07-28 22:14:48 +0200

    Fix javascript bypass

--- battle.py
+++ battle.py
@@ -4,16 +4,48 @@ import sys
 import time
 import webbrowser
 import websocket
+import re
+from selenium import webdriver
+from selenium_stealth import stealth
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def get_token(cookie=None):
     url = "https://key-drop.com/pl/token"
     if cookie:
-        headers = {
-            "cookie": f"session_id={cookie}; __vioShield=49f7a4dd63e6f34db13d25b9be83c7e5",
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        }
-        return requests.get(url, headers=headers).text
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        stealth(
+            driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+        )
+        driver.get(url)
+        driver.add_cookie(
+            {
+                "name": "session_id",
+                "value": cookie,
+            }
+        )
+        wait = WebDriverWait(driver, 10)
+        wait.until(
+            EC.text_to_be_present_in_element(
+                (By.XPATH, "//*"), "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9."
+            )
+        )
+        page_content = driver.page_source
+        driver.quit()
+        body_pattern = r"<body>(.*?)<\/body>"
+        match = re.search(body_pattern, page_content, re.DOTALL)
+        return match.group(1)
     else:
         webbrowser.open(url)
         time.sleep(1)
